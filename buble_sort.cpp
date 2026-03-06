97fd7408db657adcbacde282d0dbbbd04e7bba00
muhhafidzal <muh.hafidz.al.mubaroq@gmai.com>
2026-03-06 15:32:55 +0700

    menambah void display

--- buble_sort.cpp
+++ buble_sort.cpp
@@ -32,3 +32,16 @@ void input()
         cin >> a[i];
     }
 }
+
+void display()
+{
+    cout << endl;
+    cout << "==========================" << endl;
+    cout << "Elemen Array telah terurut" << endl;
+    cout << "==========================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke - " << i << " : " << a[i] << endl; 
+    } 
+}
